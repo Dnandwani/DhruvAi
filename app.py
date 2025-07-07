0c3283e3f786fe19bfd7cbaf1b72939bf3d14ee2
Dhruv nandwani <109612607+Dnandwani@users.noreply.github.com>
2025-07-07 13:56:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+model = genai.GenerativeModel(model_name="gemini-2.5-pro")
 chat = model.start_chat(history=[])
 
 
